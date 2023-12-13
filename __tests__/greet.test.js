4e791c4214488571a5f85b384331bc87c2fd6169
sohaib <sohaib.elbouhtani@student.vinci.be>
2023-12-13 23:26:16 +0100

    fix: lint

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,31 +1,25 @@
 import greet from '../utils/greet'
 
 test('checkgreet', () => {
-    expect(greet('Bob')).toBe('Hello, Bob.')
+  expect(greet('Bob')).toBe('Hello, Bob.')
 
-    expect(greet('')).toBe('Hello, my friend.')
+  expect(greet('')).toBe('Hello, my friend.')
 
-    expect(greet(undefined)).toBe('Hello, my friend.')
+  expect(greet(undefined)).toBe('Hello, my friend.')
 
-    expect(greet(null)).toBe('Hello, my friend.')
+  expect(greet(null)).toBe('Hello, my friend.')
 
-    expect(greet('BOB')).toBe('HELLO, BOB!')
+  expect(greet('BOB')).toBe('HELLO, BOB!')
 
-    expect(greet(['Ibrahim', 'Sohaib'])).toBe('Hello, Ibrahim and Sohaib.')
+  expect(greet(['Ibrahim', 'Sohaib'])).toBe('Hello, Ibrahim and Sohaib.')
 
-    expect(greet(['Sohaib', 'Ibrahim', 'Mohamed'])).toBe(
-        'Hello, Sohaib, Ibrahim and Mohamed'
-    ),
-        max - len
-    expect(greet(['Sohaib', 'MOHAMED', 'Ibrahim'])).toBe(
-        'Hello, Sohaib and Ibrahim. AND HELLO MOHAMED !'
-    )
+  expect(greet(['Sohaib', 'Ibrahim', 'Mohamed'])).toBe('Hello, Sohaib, Ibrahim and Mohamed')
 
-    expect(greet(['Sohaib', 'Ibrahim', 'fr'])).toBe(
-        'Bonjour Sohaib et Ibrahim.'
-    )
+  expect(greet(['Sohaib', 'MOHAMED', 'Ibrahim'])).toBe('Hello, Sohaib and Ibrahim. AND HELLO MOHAMED !')
 
-    expect(greet(['Sohaib', 'Ibrahim', 'nl'])).toBe('Hallo Sohaib en Ibrahim.')
+  expect(greet(['Sohaib', 'Ibrahim', 'fr'])).toBe('Bonjour Sohaib et Ibrahim.')
 
-    expect(greet(['Sohaib', 'Ibrahim', 'an'])).toBe('Hello Sohaib and Ibrahim.')
+  expect(greet(['Sohaib', 'Ibrahim', 'nl'])).toBe('Hallo Sohaib en Ibrahim.')
+
+  expect(greet(['Sohaib', 'Ibrahim', 'an'])).toBe('Hello Sohaib and Ibrahim.')
 })
