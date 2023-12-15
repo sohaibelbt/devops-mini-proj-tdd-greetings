3b798ac6f7e0a8c29acccd69bc1b703d0634e1df
sohaib <sohaib.elbouhtani@student.vinci.be>
2023-12-15 15:42:13 +0100

    fix: problem with prettier and eslint

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
-const config = {
-  trailingComma: "es5",
-  tabWidth: 4,
-  semi: false,
-  singleQuote: true
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
 };
 
-export default config;
+export default config
